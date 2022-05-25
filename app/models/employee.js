592e5d9adc79361bc51edc6b13caac0233ffe24b
avongalie <alexsiaavila@gmail.com>
2022-05-25 09:09:57 -0400

    created employee schema

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const exampleSchema = new mongoose.Schema({
+const employeeSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true
@@ -18,4 +18,4 @@ const exampleSchema = new mongoose.Schema({
   timestamps: true
 })
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Employee', employeeSchema)
